c14d8b547a4322234a2596748151ffb3f846c7ca
Янсон Артур <a.iansons@petrovich.ru>
2016-11-14 10:11:13 +0300

    Add browserify configuration

--- gulp/config.js
+++ gulp/config.js
@@ -1,5 +1,7 @@
 export default {
     src: 'src/**/*.js',
     out: 'scripts.js',
+    entry: 'src/components/app/app.js',
+    format: 'iife',
     dest: 'build'
 }
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,12 +2,21 @@ import gulp from 'gulp'
 import sourcemaps from 'gulp-sourcemaps'
 import babel from 'gulp-babel'
 import concat from 'gulp-concat'
+import browserify from 'gulp-browserify'
 import config from '../config'
 
 const scripts = () =>
-    gulp.src(config.src)
+    gulp.src(config.src, {
+            //base: 'src/components/app'
+            //base: '.'
+        })
         .pipe(sourcemaps.init())
         .pipe(babel())
+        // .pipe(gulp.dest(config.dest))
+        // .pipe(browserify({
+        //     insertGlobals : true,
+        //     debug : !!process.env.DEV
+        // }))
         .pipe(concat(config.out))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.dest));
@@ -15,4 +24,4 @@ const scripts = () =>
 gulp.task('scripts', scripts);
 gulp.task('watch:scripts', () =>
     gulp.watch(config.src, scripts)
-);
+)
